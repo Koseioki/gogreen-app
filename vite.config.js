f6a39154119afc4e8bd8a2fb8a3a20dfc6739a00
Kosei Oki <koseioki1225@gmail.com>
2024-12-12 13:38:57 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
     };
 
     if (command !== "serve") {
-        config.base = "/reptile-app/";
+        config.base = "/gogreen-app/";
     }
 
     return config;
