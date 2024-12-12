0e33732c69dc63f95f627fd6135b42379a005804
Kosei Oki <koseioki1225@gmail.com>
2024-12-12 13:39:44 +0100

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/reptile-app/"}>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/go-green-app/"}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
